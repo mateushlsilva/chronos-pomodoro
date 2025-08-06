d334ed4cf098b5cd990ab1f7029d8c250fa3ab92
mateushlsilva <mateushls01@gmail.com>
2025-08-06 14:05:59 -0300

    feat: Salvando o tema no localStorage e trocando o ícone

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ import { Logo } from './components/Logo'
 import { Menu } from './components/Menu'
 import { CountDown } from './components/CountDown'
 import { DefaultInput } from './components/DefaultInput'
+import { Cycles } from './components/Cycles'
+import { DefaultButton } from './components/DefaultButton'
+import { PlayCircleIcon, StopCircleIcon } from 'lucide-react'
+import { Footer } from './components/Footer'
 
 function App() {
 
@@ -28,12 +32,15 @@ function App() {
             <p>Lorem ipsum dolor sit amet.</p>
           </div>
           <div className="formRow">
-            <p>0 0 0 0 0 0</p>
+            <Cycles/>
           </div>
           <div className="formRow">
-            <button>Enviar</button>
+            <DefaultButton icon={<PlayCircleIcon />} color='green'/>
           </div>
         </form>
+      </Conteiner>
+      <Conteiner>
+        <Footer/>
       </Conteiner>
     </>
   )
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,20 +1,41 @@
-import { HouseIcon, HistoryIcon, SettingsIcon, SunIcon } from 'lucide-react'
+import { HouseIcon, HistoryIcon, SettingsIcon, SunIcon, MoonIcon } from 'lucide-react'
 import styles from './styles.module.css'
+import { useEffect, useState } from 'react'
+
+type AvailableThemes = 'dark' | 'light';
 
 export function Menu(){
+    const [ theme, setTheme ] = useState<AvailableThemes>(() => {
+        const storageTheme = localStorage.getItem('theme') as AvailableThemes || 'dark'
+        return storageTheme
+    });
+
+    const handleThemeChange = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+        event.preventDefault();        
+        setTheme(prev => {
+            const nextTheme = prev === 'dark' ? 'light' : 'dark';
+            return nextTheme
+        })
+    }
+
+    useEffect(() => {
+        document.documentElement.setAttribute('data-theme', theme)
+        localStorage.setItem('theme', theme)
+        return () => {}
+    }, [theme])
     return (
         <nav className={styles.menu}> 
-            <a className={styles.menuLink} href="#">
+            <a className={styles.menuLink} href="#" aria-label='Ir para a Home' title='Ir para a Home'>
                 <HouseIcon/>
             </a> 
-            <a className={styles.menuLink} href="#">
+            <a className={styles.menuLink} href="#" aria-label='Ver Histórico' title='Ver Histórico'>
                 <HistoryIcon/>
             </a>
-            <a className={styles.menuLink} href="#">
+            <a className={styles.menuLink} href="#" aria-label='Configurações' title='Configurações'>
                 <SettingsIcon/>
             </a>
-            <a className={styles.menuLink} href="#">
-                <SunIcon/>
+            <a className={styles.menuLink} href="#" aria-label='Mudar Tema' title='Mudar Tema' onClick={handleThemeChange}>
+                {theme === 'dark' ? <SunIcon/> : <MoonIcon/>}
             </a>
         </nav>
     )
